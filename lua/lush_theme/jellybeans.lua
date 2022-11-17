17417a122ce1cd7e09dc386ffa7c072c39a6e8c3
Jeremy Mack <mutewinter@users.noreply.github.com>
2022-11-17 11:15:13 -0600

    feat: updated jellybeans color overrides for nvim 0.8 treesitter syntax

--- lua/lush_theme/jellybeans.lua
+++ lua/lush_theme/jellybeans.lua
@@ -4,28 +4,33 @@ local hsl = lush.hsl
 local jellybeans = require('lush_theme.jellybeans-nvim')
 
 local nice_red = "#ff5656" -- A nicer red, also from https://git.io/Jfs2T
+local mantis = "#70b950" -- From Jellybeans
+local koromiko = "#ffb964" -- From Jellybeans
 
-local spec = lush.extends({jellybeans}).with(function()
+local spec = lush.extends({jellybeans}).with(function(injected_functions)
+  local sym = injected_functions.sym
+
   return {
     -- Darker background for entire window
     Normal { fg = "#e8e8d3", bg = "#090909", },
 
     -- Better colors for popup menus
     Pmenu { bg = "#202020", fg = "#D6D6D6" },
-    PmenuSel { bg = "#D6D6D6", fg = "#2B2B2B" },
+    CocMenuSel { bg = "#D6D6D6", fg = "#2B2B2B" },
 
+    -- Better colors for JSX tag attributes
+    sym("@tag.attribute.tsx") { fg = koromiko },
+
+    -- Better error color
     CocErrorSign { fg = nice_red },
 
-    -- Treesitter
-    TSTagAttribute { fg = "#ffb964" },
-
-    -- Fixes issue with template strings
-    TSVariable { fg=Normal.fg },
-
     -- Git Signs
-    GitSignsAdd { fg = "#70b950" },
+    GitSignsAdd { fg = mantis },
     GitSignsChange { fg = "#8fbfdc" },
     GitSignsDelete { fg = nice_red },
+
+    -- Specs
+    Specs { bg = mantis, fg = "#000000" },
   }
 end)
 
